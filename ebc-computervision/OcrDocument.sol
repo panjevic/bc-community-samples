fe3da474949698bdf3f93f42919c4ee1c0a92756
Bojan Panjevic <panjevicbojan@gmail.com>
2019-04-19 23:48:40 +0200

    Simplified contract. Added logic app code

--- ebc-computervision/OcrDocument.sol
+++ ebc-computervision/OcrDocument.sol
@@ -1,14 +1,18 @@
 pragma solidity >=0.5.0;
 
+// // This simple contract is used to showcase interaction with Ethereum network and should not be used as a reference point for any smart contract implementations
 contract OcrDocument {
     
-    event DocumentAdded(uint documentId, address owner, string documentUri, string textUri);
+    event DocumentAdded(uint documentId, address owner, string fileId, string name, string path, string tag, string text, bytes32 documentHash);
     
     struct Document {
-        uint DocumentId;
+        uint Id;
         address Owner;  
-        string DocumentUri;
-        string TextUri;
+        string FileId;        
+        string Name;
+        string Path;
+        string Tag;
+        string Text;
         bytes32 DocumentHash;
     }
 
@@ -16,28 +20,27 @@ contract OcrDocument {
     
     uint documentCount;
 
-    function addDocument(uint documentId, string memory documentUri, string memory textUri, bytes32 documentHash) public {
-        documentCount++;
+    function addDocument(string memory fileId, string memory name, string memory path, string memory tag, string memory text, bytes32 documentHash) public {
+        uint id = documentCount++;
 
         Document memory doc = Document({
-            DocumentId : documentId,
-            Owner : msg.sender,
-            DocumentUri : documentUri,
-            TextUri : textUri,
+            Id : id,
+            Owner : msg.sender,            
+            FileId : fileId,
+            Name : name,
+            Path : path,
+            Tag : tag,
+            Text : text,
             DocumentHash : documentHash
         });
         
-        documents[documentId] = doc;        
+        documents[id] = doc;        
         
-        emit DocumentAdded(documentId, msg.sender, documentUri, textUri);
+        emit DocumentAdded(id, msg.sender, fileId, name, path, tag, text, documentHash);
     }
 
-    function getTextUri(uint documentId) public view returns (string memory) {
-        return documents[documentId].TextUri;        
-    }
-
-    function getDocumentUri(uint documentId) public view returns (string memory) {
-        return documents[documentId].DocumentUri;        
+    function getText(uint documentId) public view returns (string memory) {
+        return documents[documentId].Text;        
     }
     
     function getCount() public view returns (uint) {
